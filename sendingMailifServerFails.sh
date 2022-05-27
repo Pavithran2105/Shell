2a536972e694b4f62673afbf19e4f12a9e2bb3b2
Pavithran2105 <88871384+Pavithran2105@users.noreply.github.com>
2022-05-27 12:02:52 +0530

    Update sendingMailifServerFails.sh

--- sendingMailifServerFails.sh
+++ sendingMailifServerFails.sh
@@ -8,7 +8,7 @@ url=$1
 if curl --output /dev/null --silent --head --fail "$url"; then
   echo "URL exists: $url"
 else
-  echo "Url doesn't exist... Kindly check it" | sendmail vinothkumars2509@gmail.com
+  echo "Url doesn't exist... Kindly check it" | sendmail pavithran.gurusamy@gmail.com
   break
 fi
  sleep 2s
